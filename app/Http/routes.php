c9e58072de0d800d8cd977f7593af8e23cfb2c3f
salsakhairinaa <salsakhairina280@gmail.com>
2018-12-04 20:53:40 +0700

    admin belom fix mo diganti

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,8 +32,13 @@ Route::get('/register', function () {
 Route::get('/login', function () {
     return view('login');
 });
+Route::get('/admin', function () {
+    return view('admin');
+    // return view('welcome');
+});
 Route::get('/login', 'Auth\AuthController@getLogin');
 Route::post('/login', 'Auth\AuthController@postLogin');
 Route::get('/logout', 'Auth\AuthController@getLogout'); 
 Route::get('/register', 'Auth\AuthController@getRegister');
 Route::post('/register', 'Auth\AuthController@postRegister');
+Route::post('/admin ', 'Auth\AuthController@postAdmin');
